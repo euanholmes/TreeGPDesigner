6555e94e5fd88e13c1b89879fa3477f6cc981e09
Euan <40454766@live.napier.ac.uk>
2023-01-04 11:09:50 +0000

    Started implementing first tutorial page.

--- TreeGPDesigner/MVVM/ViewModel/GPBasics1ViewModel.cs
+++ TreeGPDesigner/MVVM/ViewModel/GPBasics1ViewModel.cs
@@ -54,12 +54,39 @@ namespace TreeGPDesigner.MVVM.ViewModel
 
         //Non-Observable Variables
         public Node? DisplayTree;
+        public List<FunctionNode> FunctionNodes = new();
+        public List<TerminalNode> TerminalNodes = new();
+        public List<Node> RootNodes = new();
 
         public GPBasics1ViewModel()
         {
             NavHomeMenuCommand = new RelayCommand(NavHomeMenu);
             NavTutorialsMenuCommand = new RelayCommand(NavTutorialsMenu);
             GenerateTreeCommand = new RelayCommand(GenerateTree);
+
+            //Debug
+            FunctionNodes.Add(new FunctionNode("+", 2, a => a[0] + a[1], false));
+            FunctionNodes.Add(new FunctionNode("<", 2, a => a[0] < a[1] ? 1 : 0, true));
+            FunctionNodes.Add(new FunctionNode("/", 2, a => a[0] / a[1], false));
+            FunctionNodes.Add(new FunctionNode("-", 2, a => a[0] - a[1], false));
+            FunctionNodes.Add(new FunctionNode("*", 2, a => a[0] * a[1], false));
+            FunctionNodes.Add(new FunctionNode("ABS", 1, a => Math.Abs(a[0]), false));
+
+            TerminalNodes.Add(new TerminalNode("-1", 0, -1, false));
+            TerminalNodes.Add(new TerminalNode("1", 0, 1, false));
+            TerminalNodes.Add(new TerminalNode("34", 0, 34, false));
+            TerminalNodes.Add(new TerminalNode("X", 0, 0, true));
+            TerminalNodes.Add(new TerminalNode("Y", 0, 1, true));
+            TerminalNodes.Add(new TerminalNode("AB", 0, 2, true));
+
+            RootNodes.Add(new FunctionNode("+", 2, a => a[0] + a[1], false));
+            RootNodes.Add(new FunctionNode("<", 2, a => a[0] < a[1] ? 1 : 0, true));
+            RootNodes.Add(new FunctionNode(">=", 2, a => a[0] >= a[1] ? 1 : 0, true));
+            RootNodes.Add(new FunctionNode("-", 2, a => a[0] - a[1], false));
+            RootNodes.Add(new TerminalNode("X", 0, 0, true));
+            RootNodes.Add(new TerminalNode("1", 0, 1, false));
+
+            GenerateTree();
         }
 
         public void NavHomeMenu()
@@ -74,7 +101,32 @@ namespace TreeGPDesigner.MVVM.ViewModel
 
         public void GenerateTree()
         {
+            DisplayTreePlot.Clear();
+            BinPackingTemplate bpTemplate = new();
+            AddSelectedNodes(bpTemplate);
+            DisplayTree = bpTemplate.GrowTree(bpTemplate.GetRandomRootNode(), 3);
+            TreeDrawingAlgorithm.CalculateNodePositions(DisplayTree);
+            DisplayTreePlot = AppInfoSingleton.GetTreePlot(DisplayTreePlot, DisplayTree, BrushSet);
+            CanvasHeight = DisplayTree.Height * 100 / (float)0.75;
+            CanvasWidth = DisplayTree.Width * 100 / (float)0.75;
+        }
 
+        public void AddSelectedNodes(BinPackingTemplate bp)
+        {
+            foreach (FunctionNode functionNode in FunctionNodes)
+            {
+                bp.AddFunctionNode(functionNode);
+            }
+
+            foreach (TerminalNode terminalNode in TerminalNodes)
+            {
+                bp.AddTerminalNode(terminalNode);
+            }
+
+            foreach (Node rootNode in RootNodes)
+            {
+                bp.AddRootNode(rootNode);
+            }
         }
     }
 }
