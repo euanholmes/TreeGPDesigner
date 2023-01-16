4afd263cdc63e44f65db4a2558ea9ade8ac58395
Euan <40454766@live.napier.ac.uk>
2023-01-16 17:43:21 +0000

    Started working on wrappers and datasets tutorial.

--- TreeGPDesigner/MVVM/ViewModel/GPBasics3ViewModel.cs
+++ TreeGPDesigner/MVVM/ViewModel/GPBasics3ViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,34 +31,104 @@ namespace TreeGPDesigner.MVVM.ViewModel
         [ObservableProperty]
         private Brush? panel2Colour = AppInfoSingleton.Instance.CurrentPanel2Color;
 
-        //Tree Drawing Variables
-        [ObservableProperty]
-        private Brush[]? brushSet = AppInfoSingleton.Instance.CurrentBrushSet;
+        //GP Basics 3 Variables
+        private static Random random = new();
 
         [ObservableProperty]
-        private ImageSource? zoomIconSource = AppInfoSingleton.Instance.CurrentZoomIcon;
+        private int bc1 = random.Next(50, 61);
 
         [ObservableProperty]
-        public ObservableCollection<NodePlot> displayTreePlot = new();
+        private int bc2 = random.Next(50, 61);
 
         [ObservableProperty]
-        private float? canvasHeight;
+        private int bc3 = random.Next(50, 61);
 
         [ObservableProperty]
-        private float? canvasWidth;
+        private int bc4 = random.Next(50, 61);
+
+        private List<int> items1 = new() /*{ random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), 
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) }*/;
+
+        private List<int> items2 = new() /*{ random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61),
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) }*/;
+
+        private List<int> items3 = new() /*{ random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61),
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) }*/;
+
+        private List<int> items4 = new() /*{ random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61), random.Next(10, 61),
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) }*/;
+       
+        
+
+        [ObservableProperty]
+        private string items1String;
+
+        [ObservableProperty]
+        private string items2String;
+
+        [ObservableProperty]
+        private string items3String;
+
+        [ObservableProperty]
+        private string items4String;
+
+        private List<List<int>> bins1;
+        private List<List<int>> bins2;
+        private List<List<int>> bins3;
+        private List<List<int>> bins4;
+
+        private ObservableCollection<Bin> binList1 = new();
+        private ObservableCollection<Bin> binList2 = new();
+        private ObservableCollection<Bin> binList3 = new();
+        private ObservableCollection<Bin> binList4 = new();
+
+        private BinPackingTemplate bpTemplate = new();
+
+        [ObservableProperty]
+        private ObservableCollection<Bin> bins = new();
+
+        [ObservableProperty]
+        private int currentBinList = 0;
+
 
         //Commands
         public ICommand NavHomeMenuCommand { get; }
         public ICommand NavTutorialsMenuCommand { get; }
         public ICommand NavPreviousCommand { get; }
         public ICommand NavNextCommand { get; }
+        public ICommand RunWrapperCommand { get; }
+        public ICommand RadioButtonCommand { get; }
 
+        //Constructor
         public GPBasics3ViewModel()
         {
             NavHomeMenuCommand = new RelayCommand(NavHomeMenu);
             NavTutorialsMenuCommand = new RelayCommand(NavTutorialsMenu);
             NavPreviousCommand = new RelayCommand(NavPrevious);
             NavNextCommand = new RelayCommand(NavNext);
+            RunWrapperCommand = new RelayCommand(RunWrapper);
+            RadioButtonCommand = new RelayCommand<string>(param => RadioButton(param));
+
+            items1 = new() { random.Next(10, Bc1), random.Next(10, Bc1), random.Next(10, Bc1), random.Next(10, Bc1), random.Next(10, Bc1), random.Next(10, Bc1), 
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) };
+            items2 = new() { random.Next(10, Bc2), random.Next(10, Bc2), random.Next(10, Bc2), random.Next(10, Bc2), random.Next(10, Bc2), random.Next(10, Bc2),
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) };
+            items3 = new() { random.Next(10, Bc3), random.Next(10, Bc3), random.Next(10, Bc3), random.Next(10, Bc3), random.Next(10, Bc3), random.Next(10, Bc3),
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) };
+            items4 = new() { random.Next(10, Bc4), random.Next(10, Bc4), random.Next(10, Bc4), random.Next(10, Bc4), random.Next(10, Bc4), random.Next(10, Bc4),
+            random.Next(1, 10), random.Next(1, 10), random.Next(1, 10), random.Next(1, 10) };
+
+            Items1String = ConvertItemsToString(items1);
+            Items2String = ConvertItemsToString(items2);
+            Items3String = ConvertItemsToString(items3);
+            Items4String = ConvertItemsToString(items4);
+
+            bins1 = bpTemplate.BPOfflineWrapper(items1, Bc1, bpTemplate.KnownAlgorithms[0]);
+            bins2 = bpTemplate.BPOfflineWrapper(items2, Bc2, bpTemplate.KnownAlgorithms[0]);
+            bins3 = bpTemplate.BPOfflineWrapper(items3, Bc3, bpTemplate.KnownAlgorithms[0]);
+            bins4 = bpTemplate.BPOfflineWrapper(items4, Bc4, bpTemplate.KnownAlgorithms[0]);
+
+            GetBinLists();
         }
 
         //Navigation Functions
@@ -79,5 +150,129 @@ namespace TreeGPDesigner.MVVM.ViewModel
         {
             AppInfoSingleton.Instance.CurrentViewModel = new GPBasics4ViewModel();
         }
+
+        //GP Basics 3 Functions
+        public void RunWrapper()
+        {
+            Bins.Clear();
+
+            if (CurrentBinList == 0)
+            {
+                Bins = new ObservableCollection<Bin>(binList1);
+            }
+            else if (CurrentBinList == 1)
+            {
+                Bins = new ObservableCollection<Bin>(binList2);
+            }
+            else if (CurrentBinList == 2)
+            {
+                Bins = new ObservableCollection<Bin>(binList3);
+            }
+            else
+            {
+                Bins = new ObservableCollection<Bin>(binList4);
+            }
+        }
+
+        public void RadioButton(string radioButtonNumString)
+        {
+            int radioButtonNum = Convert.ToInt32(radioButtonNumString);
+            currentBinList = radioButtonNum;
+        }
+
+        public void GetBinLists()
+        {
+            binList1 = GetBinList(bins1);
+            binList2 = GetBinList(bins2);
+            binList3 = GetBinList(bins3);
+            binList4 = GetBinList(bins4);
+        }
+
+        public ObservableCollection<Bin> GetBinList(List<List<int>> rawBinList)
+        {
+            ObservableCollection<Bin> binList = new();
+
+            for (int i = 0; i < rawBinList.Count; i++)
+            {
+                Bin bin = new();
+                bin.TextColour = TextColour;
+                for (int j = 0; j < rawBinList[i].Count; j++)
+                {
+                    bin.BinItems.Add(new BinItem(rawBinList[i][j], bin.TextColour));
+                }
+                binList.Add(bin);
+            }
+
+            return binList;
+        }
+
+        public string ConvertItemsToString(List<int> itemsList)
+        {
+            string itemsString = "";
+
+            for (int i = 0; i < itemsList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    itemsString += itemsList[i];
+                }
+                else
+                {
+                    itemsString += ", " + itemsList[i];
+                }
+            }
+
+            return itemsString;
+        }
     }
+
+    public class BinItem
+    {
+        private int currentBinItem;
+        private Brush textColour;
+
+        public BinItem(int currentBinItem, Brush textColour)
+        {
+            this.currentBinItem = currentBinItem;
+            this.textColour = textColour;
+        }
+
+        public int CurrentBinItem { get => currentBinItem; set => currentBinItem = value; }
+        public Brush TextColour { get => textColour; set => textColour = value; }
+    }
+
+    public class Bin
+    {
+        private List<BinItem> binItems = new();
+        private Brush textColour;
+
+        public Bin()
+        {
+
+        }
+
+        public Bin(List<BinItem> binItems, Brush textColour)
+        {
+            this.binItems = binItems;
+            this.textColour = textColour;
+        }
+
+        public List<BinItem> BinItems { get => binItems; set => binItems = value; }
+        public Brush TextColour { get => textColour; set => textColour = value; }
+    }
+
+    /*public class BinList
+    {
+        private Bin currentBin;
+        private Brush textColour;
+
+        public BinList(Bin currentBin, Brush textColour)
+        {
+            this.currentBin = currentBin;
+            this.textColour = textColour;
+        }
+
+        public Bin CurrentBin { get => currentBin; set => currentBin = value; }
+        public Brush TextColour { get => textColour; set => textColour = value; }
+    }*/
 }
